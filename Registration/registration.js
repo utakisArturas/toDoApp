d817df7afb0836c76b7c81d74f57b3d5f947ae24
RRokas <rokas.ringaila@gmail.com>
2022-05-23 20:22:00 +0300

    registration js without redirect

---
+++ Registration/registration.js
@@ -0,0 +1,34 @@
+const emailInput = document.getElementById("email");
+const firstNameInput = document.getElementById("nameInput");
+const lastNameInput = document.getElementById("lastNameInput");
+
+function clearInputs(){
+    emailInput.value = "";
+    firstNameInput.value = "";
+    lastNameInput.value = "";
+}
+
+function register(){
+    let formData = {
+        email: emailInput.value,
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value
+    }
+
+    fetch("https://testapi.io/api/wehevov449/resource/toDoAppUsers",{
+        method: "POST",
+        headers: {'Content-type': "application/json"},
+        body: JSON.stringify(formData)
+    })
+    .then(function(result){
+        if (result.ok === true){
+            alert("User created.");
+            // redirect to logged in view
+            // window.location.href = "";
+        }
+        else{
+            clearInputs();
+            alert("API error.")
+        }
+    })
+}
