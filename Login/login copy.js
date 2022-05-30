3335c31b584ebe5c707d463a593d0f02f8bcbbec
utakisArturas <utakis.arturas.github@gmail.com>
2022-05-30 22:16:29 +0300

    test host nr2
    test host nr2

---
+++ Login/login copy.js
@@ -0,0 +1,46 @@
+const firstNameInput = document.getElementById("nameInput");
+const lastNameInput = document.getElementById("lastNameInput");
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("click", () =>{
+    let error = document.getElementById("error");
+    if (error != null){
+        error.remove();
+    }
+});
+
+if (sessionStorage.getItem("loggedInUserEmail") != null){
+    window.location.href = "/ToDo/ToDo.html";
+}
+
+function login(){
+    fetch("https://testapi.io/api/wehevov449/resource/toDoAppUsers")
+        .then((response) => {return response.json()})
+        .then((data) => {
+            let users = data.data;
+            let loggedInUser = users.filter(matchesLoginFormData);
+
+            if (loggedInUser.length === 0){
+                let error = document.createElement("span");
+                error.innerText = "Combination does not exist!";
+                error.setAttribute("id", "error");
+                error.style.color = "red";
+                lastNameInput.insertAdjacentElement("afterend", error);
+                firstNameInput.value = "";
+                lastNameInput.value = "";
+            } else if (loggedInUser.length > 1){
+                alert("Database error, multiple users found!");
+            } else {
+                window.sessionStorage.setItem("loggedInUserEmail", loggedInUser[0].email);
+                window.location.href = "/ToDo/ToDo.html";
+            }
+        })
+}
+
+function matchesLoginFormData(user){
+    return (
+        (firstNameInput.value === user.firstName) 
+        && 
+        (lastNameInput.value === user.lastName)
+        )
+}
